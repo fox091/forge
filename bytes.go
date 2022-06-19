ea1d19f9b2feeeb1711126f883ddaab11dfc70f1
Josh Massey <joshmassey25@gmail.com>
2022-06-19 14:46:15 -0400

    Add doc comment to Bytes function

--- bytes.go
+++ bytes.go
@@ -4,6 +4,9 @@ type BytesOptions struct {
 	Count int
 }
 
+// Bytes returns a slice of random bytes.
+//
+// By default, it will give you 5 bytes, but you can specify the count you want.
 func Bytes(optionModifiers ...func(*BytesOptions)) []byte {
 	rand := getRand()
 	options := BytesOptions{
